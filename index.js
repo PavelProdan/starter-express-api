8e06c424246382c2d911d78d46ab4775b8de3a30
Pavel Prodan <35011979+PavelProdan@users.noreply.github.com>
2023-07-29 13:49:19 +0300

    news

--- index.js
+++ index.js
@@ -11,27 +11,15 @@ app.post("/stockUpdated", (req, res) => {
     res.status(200).end()
 })
 
-app.post("/testapi", (req, res) => {
-    console.log("I got a request")
-    // wait for one second before responding
-    setTimeout(() => {
-        // return a random number between 1 and 100 as request result
-        console.log(req.body)
-        res.json({result: Math.ceil(Math.random() * 100)})
-
-    }, 10)
-})
-
 app.get("/cyclicapi", (req, res) => {
     
-    for(let i = 0; i < 100; i++){
+    for(let i = 0; i < 10; i++){
         fetch('https://dummyjson.com/products/1')
         .then(res => res.json())
         .then(json => console.log(json));
 
     }
     
-
     res.status(200).end(JSON.stringify({"status":"success","message":"ok"}))
 })
 
