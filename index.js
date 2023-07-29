75694132deea1dfeb36ff86b777d2b95a17c8e64
Pavel Prodan <35011979+PavelProdan@users.noreply.github.com>
2023-07-29 16:49:17 +0300

    added axios

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios');
 const bodyParser = require("body-parser")
 
 const app = express()
@@ -12,16 +13,21 @@ app.post("/stockUpdated", (req, res) => {
 })
 
 app.get("/cyclicapi", (req, res) => {
-    
-    for(let i = 0; i < 10; i++){
-        fetch('https://dummyjson.com/products/1')
-        .then(res => res.json())
-        .then(json => console.log(json));
-
-    }
-    
-    res.status(200).end(JSON.stringify({"status":"success","message":"ok"}))
+    // get some josn response from a dummy data source and retunr the product id
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).end(JSON.stringify({"product_id": "1234"}))
 })
+
+app.get('/testcyclic', async (req, res) => {
+    try {
+      const response = await axios.get('https://dummyjson.com/products/1');
+      console.log(response.data); // Log the fetched data to the console
+      res.json(response.data); // Send the data as a JSON response to the client
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+      res.status(500).json({ error: 'Failed to fetch data' });
+    }
+  });
 
 
 app.post("/workerStock", (req, res) => {
